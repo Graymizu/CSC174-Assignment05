f3221f57400f18e9647decc72d9467ac130422c0
dragon1472 <dragon1472.0@gmail.com>
2017-09-30 16:33:40 -0400

    Add files via upload

---
+++ astruhl.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Team Shanghai, CSC 174 Assignment 4</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	
+								
+	</head>
+
+	<body>		
+		<header class="page-header">
+			<h1>
+				<a href="index.php">
+					<img src="images/logo.jpg" alt="Logo"></a>
+						<a class="logo" href="index.php"><strong> Web Developers</strong></a>
+			</h1>	
+				<p>CSC 174 - Advanced Front-End Web Development</p>
+						
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="phoinguyen.php">Phoi Nguyen</a></li>
+					<li><a href="lunguyen.php">Lu Nguyen</a></li>
+					<li><a class="active" href="astruhl.php">Adam Struhl</a></li>
+
+				</ul>
+			</nav>					
+		</header>
+
+		<section>
+		<h4>Adam Struhl</h4>
+
+		<h2>Education</h2>
+
+			<p>I am currently a senior at the University of Rochester, Class of 2018. I am majoring in <em>Digitial Media Studies</em> and <em>Creative writing</em>. I enjoy creating things that look cool to me, which is esentially things that look good to my design aesthetic.  I'm a pretty big fan of Sci-Fi, whether it be ultra-modern or cyberpunk, and I think that the former influences my personal aesthetics more than a little.</p>
+
+		<h2>Work Experiences</h2>
+
+			<p>I have spent some time as an intern at the web company WebnBeyond, as well as few contract jobs for Magic Masters signs that required a lot of graphics design work and organization.</p>
+
+		<h3>Clubs and Organization</h3>
+
+			<p>I am a long-standing member fo the Anime Interest Floor.  I spent my sophmore year working as their PR manager before becoming the President of the organization in my Junior year.  I am currently serving a second term as president, and am working towards improving the group by increasing the number of cool and unique events that we do.</p>
+		</section>	
+			<?php include "myinclude.inc" ?>			
+	
+		<?php include "footer.inc" ?>			
+
+	</body>
+</html>
