f246aae21f311dd0870c066c01102b7a67b988ab
Graymizu <graymizu@gmail.com>
2017-09-30 15:47:39 -0400

    cleaned up anthony.php to a basic easy to edit form

--- anthony.php
+++ anthony.php
@@ -1,32 +1,31 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>Anthony Russell</title>
 </head>
 <body>
-	<div class="excerpt">
-		<nav class="menu">
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<div class="people-menu">
+	<div>
+		<nav>
+			<div>
+				<ul>
+					<li><a href="index.php">Home</a></li>
 					<li><a href="lulu.php">Lulu</a></li>
 					<li><a href="anthony.php">Anthony</a></li>
-					<li><a href="shareef.php">Shareef</a></li>
-				</div>
-			</ul>
+				</ul>
+			</div>
 		</nav>
 	</div>
 
-	<div class="overview">
+	<div> <!--Contains everyting except for the navigation and title-->
 
-		<div class="excerpt">
+		<div> <!--Contains page title-->
 
 			<h1> Anthony Russell </h1>
 
-		</div>
+		</div> <!--End of title-->
 
-		<div class="excerpt">
+		<div> <!--Contains about me-->
 			<h2>
 				About Me
 			</h2>
@@ -35,48 +34,48 @@
 					Hi, name is Anthony and I'm a senior at the University of Rochester. I belive my time at college has been largely shaped by the clubs that I've joined and material that I've studied. I currently hold the Business Manager Eboard position for RICE Crew, and I was on the Hospitality Team for ACF during my junior year.
 				</p>	
 
-		</div>
+		</div> <!--End of about me-->
 
-		<div class="excerpt">
+		<div> <!--Contains firts bit of information-->
 
 			<h2>My Academics</h2>
 
-			<h3>Computer Science</h3>
+				<h3>Computer Science</h3>
 
-			<p>My first major is CS. I knew that I was going to pursue this since coming to Rochester. I doing a BA with a focus on Human Computer Interaction.</p>
+					<p>My first major is CS. I knew that I was going to pursue this since coming to Rochester. I doing a BA with a focus on Human Computer Interaction.</p>
 
-			<h3>Japanese</h3>
+				<h3>Japanese</h3>
 
-			<p>My second major is Japanese Language. I have always had an interest in the Japanese languade. When I first came to Rochester this started out as a minor, but I decided to go for the major when I realized that I didn't want to only focus on computer science.</p>
+					<p>My second major is Japanese Language. I have always had an interest in the Japanese languade. When I first came to Rochester this started out as a minor, but I decided to go for the major when I realized that I didn't want to only focus on computer science.</p>
 
-			<h3>Linguistics</h3>
+				<h3>Linguistics</h3>
 
-			<p>My cluster is in Linguistics. I fiured it would be cool to study language even more.</p>
+					<p>My cluster is in Linguistics. I fiured it would be cool to study language even more.</p>
 
-		</div>
+		</div> <!--End of 1st bit of information-->
 
-		<div class="excerpt">
-			<h2>My Clubs</h2>
+		<div> <!--Contains second bit of information-->
 
-			<h3>ACF</h3>
+			<h2>My Clubs</h2> 
 
-			<p>
-				AGAPE Christian Fellowship. We are a multi-ethnic student group devoted to learning, understanding, and applying the Word of God into our lives. We seek to impact our community and culture through various service projects and volunteer involvement opportunities.
-			</p>
+				<h3>ACF</h3>
 
-			<h3>RICE Crew</h3>
+					<p>
+						AGAPE Christian Fellowship. We are a multi-ethnic student group devoted to learning, understanding, and applying the Word of God into our lives. We seek to impact our community and culture through various service projects and volunteer involvement opportunities.
+					</p>
 
-			<p>
-				Rochester International Choreography Entertainment Crew, a committee of KASA, promotes the cultural aspects of Korean pop dance (Kpop) and music while integrating it with its American hip hop counterparts. We hope to promote the similarities and link between the two genres. RICE Crew performs in KASA and other campus organizations' events as an illustration of Korean pop dance and music. Anyone who is interested in learning Korean and American hip hop dances are welcomed regardless of ethnicity.
-			</p>
+				<h3>RICE Crew</h3>
 
-			<h3>JSA</h3>
+					<p>
+						Rochester International Choreography Entertainment Crew, a committee of KASA, promotes the cultural aspects of Korean pop dance (Kpop) and music while integrating it with its American hip hop counterparts. We hope to promote the similarities and link between the two genres. RICE Crew performs in KASA and other campus organizations' events as an illustration of Korean pop dance and music. Anyone who is interested in learning Korean and American hip hop dances are welcomed regardless of ethnicity.
+					</p>
 
-			<p>
-				Japanese Students' Association (JSA) provides the University of Rochester community with an opportunity to experience and learn both Japanese language and culture. Open to anyone interested in Japan, JSA offers activities and events teaching traditional arts, pop-culture, and various other points of Japanese culture.
-			</p>
+				<h3>JSA</h3>
 
-		</div>
-	</div>
-</body>
+					<p>
+						Japanese Students' Association (JSA) provides the University of Rochester community with an opportunity to experience and learn both Japanese language and culture. Open to anyone interested in Japan, JSA offers activities and events teaching traditional arts, pop-culture, and various other points of Japanese culture.
+					</p>
+
+		</div> <!--End of 2nd info-->
+	</div> <!--End of main content-->
 </html>
