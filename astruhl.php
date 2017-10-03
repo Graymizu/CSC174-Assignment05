b97975731a8cc58e00fea9ce2f4d3b03de833c6d
dragon1472 <dragon1472.0@gmail.com>
2017-10-02 20:06:13 -0400

    Changed to fit the format
    Edited the head to fit the uniform format of the pages, and cleaned the lines out

--- astruhl.php
+++ astruhl.php
@@ -1,58 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+	<title>Adam Struhl-Team Scotland Yard Assignment 5</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Latest compiled and minified CSS of Bootstrap from w3c -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
 
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<!-- Latest compiled and minified CSS of Bootstrap from w3c -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		
-		<title>Team Shanghai, CSC 174 Assignment 4</title>
-		
-	
-								
-	</head>
+<body>		
+	<!--added a php include for the nav bar to improve universality-->
+	<?php include "nav.inc" ?>	
 
-	<body>		
-		<header class="page-header">
-			<h1>
-				<a href="index.php">
-					<img src="images/logo.jpg" alt="Logo"></a>
-						<a class="logo" href="index.php"><strong> Web Developers</strong></a>
-			</h1>	
-				<p>CSC 174 - Advanced Front-End Web Development</p>
-						
-	 <!--Contains navigation bar-->
-			<nav>
-				<ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a href="astruhl.php">Adam Struhl</a></li>
-					<li><a href="anthony.php">Anthony Russell</a></li>
-					<li><a href="lulu.php">Lulu Chen</a></li>
-				</ul>
-			</nav>
-				
-		</header>
-
-		<section>
-		<h4>Adam Struhl</h4>
+		<h1>Adam Struhl</h1>
 
 		<h2>Education</h2>
-
-			<p>I am currently a senior at the University of Rochester, Class of 2018. I am majoring in <em>Digitial Media Studies</em> and <em>Creative writing</em>. I enjoy creating things that look cool to me, which is esentially things that look good to my design aesthetic.  I'm a pretty big fan of Sci-Fi, whether it be ultra-modern or cyberpunk, and I think that the former influences my personal aesthetics more than a little.</p>
+			<p>
+				I am currently a senior at the University of Rochester, Class of 2018. I am majoring in <em>Digitial Media Studies</em> and <em>Creative writing</em>. I enjoy creating things that look cool to me, which is esentially things that look good to my design aesthetic.  I'm a pretty big fan of Sci-Fi, whether it be ultra-modern or cyberpunk, and I think that the former influences my personal aesthetics more than a little.
+			</p>
 
 		<h2>Work Experiences</h2>
+			<p>
+				I have spent some time as an intern at the web company WebnBeyond, as well as few contract jobs for Magic Masters signs that required a lot of graphics design work and organization.
+			</p>
 
-			<p>I have spent some time as an intern at the web company WebnBeyond, as well as few contract jobs for Magic Masters signs that required a lot of graphics design work and organization.</p>
+		<h2>Clubs and Organization</h2>
+			<p>
+				I am a long-standing member of the Anime Interest Floor.  I spent my sophmore year working as their PR manager before becoming the President of the organization in my Junior year.  I am currently serving a second term as president, and am working towards improving the group by increasing the number of cool and unique events that we do.
+			</p>
 
-		<h3>Clubs and Organization</h3>
-
-			<p>I am a long-standing member fo the Anime Interest Floor.  I spent my sophmore year working as their PR manager before becoming the President of the organization in my Junior year.  I am currently serving a second term as president, and am working towards improving the group by increasing the number of cool and unique events that we do.</p>
-		</section>	
-			<?php include "myinclude.inc" ?>			
-	
-		<?php include "footer.inc" ?>			
-
-	</body>
+</body>
 </html>
