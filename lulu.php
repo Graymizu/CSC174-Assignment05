64f5fbf0e976f506dd46bfacd4d17c2e62b4676c
dragon1472 <dragon1472.0@gmail.com>
2017-10-02 21:23:23 -0400

    added footer

--- lulu.php
+++ lulu.php
@@ -40,6 +40,6 @@
 				<p>
 					I transferred to the University of Rochester in the year of 2014. However, during my time at my other college, I took a couple of Graphic Design courses and fell in love with it. We did not code much, but we played around with formatting and 3D design. 
 				</p>
-
+<?php include "footer.inc" ?>	
 </body>
 </html>
