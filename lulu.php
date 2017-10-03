6576a7357ceadec1d6c3ce114c0693edae4e1dab
dragon1472 <dragon1472.0@gmail.com>
2017-10-02 20:07:26 -0400

    Cleaned up Header and code

--- lulu.php
+++ lulu.php
@@ -1,49 +1,38 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-
+	<title>Yingju Chen-Team Scotland Yard Assignment 5</title>									
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title> Yingju Chen </title>
-
+	<!-- Latest compiled and minified CSS of Bootstrap from w3c -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
+
 <body>
+	<!--added a php include for the nav bar to improve universality-->
+	<?php include "nav.inc" ?>	
+		<h1>Yingju Chen</h1>
 
-<h1>
-	Yingju Chen
-</h1>
-
-<h2>
-	About Me
-</h2>
-
-	<p>
-		Hello! My name is Lulu and I am a third year at the University of Rochester. Currently, I am working on a independent study at the Saunders Research Building located at the hospital. I have experience as a field worker with people of disabilities. I am pursuing a minor in Computer Science because it will be beneficial for my future. I used to be a Graphic Design Major and still love to play around with code and formatting during my free time. 
-	</p>
+			<h2>About Me</h2>
+				<p>
+					Hello! My name is Lulu and I am a third year at the University of Rochester. Currently, I am working on a independent study at the Saunders Research Building located at the hospital. I have experience as a field worker with people of disabilities. I am pursuing a minor in Computer Science because it will be beneficial for my future. I used to be a Graphic Design Major and still love to play around with code and formatting during my free time. 
+				</p>
 	
-<h2>
-	TOUR 
-</h2>
+			<h2>TOUR</h2>
+				<p>
+					I am a academic coach for the program called TOUR. It stands for Transitional Opportunties for students at University of Rochester. 
+				</p>
+				
+				<h3>My Role</h3>
+					<p>
+						As a academic coach I audit a course with a student in TOUR and we take a college course together. I also meet with them outside of class time for a hour or so every week to help them keep up in the class. As a coach, my primary job is to help my mentee reach the goal that they want for the particular class they are taking. 
+					</p>
 
-	<p>
-		I am a academic coach for the program called TOUR. It stands for Transitional Opportunties for students at University of Rochester. 
-	</p>
-	<h3>
-		My Role
-	</h3>
-		<p>
-			As a academic coach I audit a course with a student in TOUR and we take a college course together. I also meet with them outside of class time for a hour or so every week to help them keep up in the class. As a coach, my primary job is to help my mentee reach the goal that they want for the particular class they are taking. 
-		</p>
-
-
-<h2>
-	Graphic Design 
-</h2>
-
-	<p>
-		I transferred to the University of Rochester in the year of 2014. However, during my time at my other college, I took a couple of Graphic Design courses and fell in love with it. We did not code much, but we played around with formatting and 3D design. 
-	</p>
-
+			<h2>Graphic Design</h2>
+				<p>
+					I transferred to the University of Rochester in the year of 2014. However, during my time at my other college, I took a couple of Graphic Design courses and fell in love with it. We did not code much, but we played around with formatting and 3D design. 
+				</p>
 
 </body>
 </html>
