5eb4fe990d63241819df2541440f8272af5f4a28
dragon1472 <dragon1472.0@gmail.com>
2017-10-02 21:09:53 -0400

    added index for a homepage

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Team Scotland Yard Assignment 5</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Latest compiled and minified CSS of Bootstrap from w3c -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+
+<body>		
+		
+	<!--Navigation bar-->
+	<?php include "nav.inc" ?>			
+
+		<header>
+			<h1>Team Scotland Yard</h1>				
+		</header>
+
+		<section>
+			<article>
+				<h2><em>Adam Struhl - Information Architect</em></h2>
+				<a href="astruhl.php">
+					<figure>
+					<img src="images/architect.jpg" alt="Adam Struhl">
+					</figure>
+				</a>
+				<a href="astruhl.php"> Read more...</a>
+			</article>
+				
+			<article>
+				<h2><em>Anthony Russell - Design Artist</em></h2>
+					<a href="anthony.php">
+					<figure>
+					<img src="images/design.png" alt="Anthony Russell">
+					</figure>
+					</a>
+				<a href="anthony.php">Read more...</a>
+			</article>
+
+			<article>
+				<h2><em>Yingju Chen - Code Monkey</em></h2>
+					<a href="lulu.php">
+					<figure>
+					<img src="images/monkey.jpg" alt="Yinju Chen">
+					</figure>
+					</a>
+				<a href="lulu.php"> Read more... </a>
+			</article>
+
+		</section>
+	
+	</body>
+</html>
