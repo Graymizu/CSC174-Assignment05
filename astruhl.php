61a9b76de48baf2792b8e2a98a715df110f615aa
dragon1472 <dragon1472.0@gmail.com>
2017-10-02 20:32:54 -0400

    added images and edited work experiences text

--- astruhl.php
+++ astruhl.php
@@ -14,18 +14,33 @@
 	<?php include "nav.inc" ?>	
 
 		<h1>Adam Struhl</h1>
+			<figure>
+				<img src="images/adam.jpg" alt="Adam Struhl">
+			</figure>
 
 		<h2>Education</h2>
+			<figure>
+				<img src="images/school.jpg" alt="Education">
+			</figure>
 			<p>
 				I am currently a senior at the University of Rochester, Class of 2018. I am majoring in <em>Digitial Media Studies</em> and <em>Creative writing</em>. I enjoy creating things that look cool to me, which is esentially things that look good to my design aesthetic.  I'm a pretty big fan of Sci-Fi, whether it be ultra-modern or cyberpunk, and I think that the former influences my personal aesthetics more than a little.
 			</p>
 
 		<h2>Work Experiences</h2>
+			<figure>
+				<img src="images/work.png" alt="Work Experience">
+			</figure>
 			<p>
-				I have spent some time as an intern at the web company WebnBeyond, as well as few contract jobs for Magic Masters signs that required a lot of graphics design work and organization.
+				I have spent some time as an intern at the web company WebnBeyond, where I did work on presentation decks and several pieces of HTML coding and formatting.
+			</p>
+			<p>
+				I have also done a few contract jobs for Magic Masters signs, most of which were in the fields of grahpic design and instruction manual creation.
 			</p>
 
 		<h2>Clubs and Organization</h2>
+			<figure>
+				<img src="images/club.jpg" alt="Clubs">
+			</figure>
 			<p>
 				I am a long-standing member of the Anime Interest Floor.  I spent my sophmore year working as their PR manager before becoming the President of the organization in my Junior year.  I am currently serving a second term as president, and am working towards improving the group by increasing the number of cool and unique events that we do.
 			</p>
